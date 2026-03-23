95b85e5c435772a05f8bf3d1518deda0c8d36c7b
Priyank Kumar <pkrjsap@gmail.com>
2026-03-23 19:10:40 +0000

    Filter in Ui5 and Odata

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,22 +1,37 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "com/pjnamespace/project1/model/formatter"
-
+    "com/pjnamespace/project1/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ], 
-function (Controller,formatter)  {
+function (Controller, formatter, Filter, FilterOperator) {
     "use strict";
+
     return Controller.extend("com.pjnamespace.project1.controller.View1", {
-        f:formatter,
-        onInit() {
+
+        formatter: formatter,
+
+        onInit: function () {
         },
-        
-     onSubmit: function () {
-    
-       var select = this.getView().byId("select").getSelectedKey();
-       var combo = this.getView().byId("combo").getSelectedKeys();
-       var radio = this.getView().byId("radio").getSelectedIndex();
 
+        onSubmit: function () {
+            var select = this.getView().byId("select").getSelectedKey();
+            var combo = this.getView().byId("combo").getSelectedKeys();
+            var radio = this.getView().byId("radio").getSelectedIndex();
+        },
 
+        onGo: function () {
+            var aFilter = [];
+            var customer = this.byId("customer").getValue();
+            if (customer !== "") {
+                aFilter.push(new Filter("CustomerId", "EQ", customer));
+            }
+            this.byId("table").getBinding("items").filter(aFilter)            
+        },
+        onReset: function () {
+            this.byId("customer").setValue("");
+            this.byId("agency").setValue("");
         }
+
     });
 });
